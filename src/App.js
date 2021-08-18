49873d5bc9c0329485b30d8954f2f8987f71fc5d
gyuho <hagyuho90@gmail.com>
2021-08-18 17:27:27 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, {Component}from 'react';
+import PhoneForm from './Components/PhoneForm';
+
+class App extends Component {
+
+  id = 0;
+
+
+  state = {
+    information:[],
+  }
+
+  handleCreate = (data) =>{
+    const{information} = this.state;
+    this.setState({
+      information: information.concat(Object.assign({},data,{
+        id: this.id++
+      }))
+    })
+  } 
+
+  render(){
+    return(
+      <div>
+        <PhoneForm onCreate={this.handleCreate}/>
+        {JSON.stringify(this.state.information)}
+      </div>
+    )
+  }
+}
+
+export default App;
